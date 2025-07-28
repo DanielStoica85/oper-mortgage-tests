27e15c6e635df2d9a71d77563bf75d7458a9fe6c
Daniel Stoica <dldanielstoica@gmail.com>
2025-07-28 19:19:24 +0200

    Add test data file.

---
+++ data/mortgageSimulationData.ts
@@ -0,0 +1,15 @@
+export const mortgageSimulationData = {
+  ownFunds: "100000",
+  salary: "4000",
+  rent: "800",
+  birthDate: "13/05/1985",
+  dependents: "0",
+  projectDetails: {
+    propertyPrice: "300000",
+    typeOfProperty: "House",
+    projectLocation: "Brussels",
+    propertyUsedFor: "Living",
+    typeOfSale: "Private sale",
+    epcScore: "10",
+  },
+};
